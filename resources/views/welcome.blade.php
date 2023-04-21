06ceda1c68510c7f3676e16b60d29573281ef82b
Sharif Hasan <me.sharif.hasan@gmail.com>
2023-04-21 13:33:59 +0600

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <title>The Advance calculator</title>
+    <link href="{{asset("css/bootstrap.min.css")}}" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="monitor col-md-12 col-sm-12 col-lg-12">Start calculating</div>
+        </div>
+    </div>
+<script>
+    let btn=document.querySelector("#clickme");
+    let i=0;
+    btn.addEventListener("click",function (){
+        let disp=document.querySelector("#display");
+        disp.innerHTML="Clicked "+(i++);
+    })
+</script>
+</body>
+</html>
